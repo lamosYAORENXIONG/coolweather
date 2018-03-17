4d5948c6563f47c2d5803f663f499bb6fdb77285
lamos <msm8274ac@163.com>
2018-03-17 20:53:36 +0800

    create Database

---
+++ app/src/main/java/com/example/lamos/db/Provice.java
@@ -0,0 +1,41 @@
+package com.example.lamos.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by lamos on 2018/3/17.
+ */
+
+public class Provice extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+
+}
